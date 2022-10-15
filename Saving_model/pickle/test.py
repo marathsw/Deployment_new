75744a6e25d27babe6b692cb43ed2ceb7ba8c468
marathsw <swatej@hotmail.com>
2022-10-15 16:26:49 +0530

    Init

---
+++ Saving_model/pickle/test.py
@@ -0,0 +1,8 @@
+import pickle
+model=pickle.load(open('diabetes_79.pkl','rb'))
+result=model.predict([[0,1,1,1,1,1,1,0]])
+
+if result[0]==1:
+    print("diabetic")
+else:
+    print("Not diabetic")
